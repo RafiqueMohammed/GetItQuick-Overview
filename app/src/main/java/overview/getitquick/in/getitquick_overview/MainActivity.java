6c02b45a0fab6e68cfd344b5ba245fdb2f449190
Rafique Mohammed <dev.rafiq@live.com>
2014-09-19 19:31:39 +0530

    Registration Page Added

--- app/src/main/java/overview/getitquick/in/getitquick_overview/MainActivity.java
+++ app/src/main/java/overview/getitquick/in/getitquick_overview/MainActivity.java
@@ -1,17 +1,30 @@
 package overview.getitquick.in.getitquick_overview;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+    TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        register= (TextView) findViewById(R.id.txt_register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(MainActivity.this,Register.class);
+                startActivity(i);
+
+            }
+        });
     }
 
 
